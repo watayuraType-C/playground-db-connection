ea67e0563c89d4e04dadd4a1430e7be589135860
wata <watayurayokohama@gmail.com>
2026-01-12 19:25:34 +0900

    まっさらな状態に戻します。

--- src/lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// .env.localから設定を読み込む
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-// Supabaseと通信するための「窓口」を作る
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
